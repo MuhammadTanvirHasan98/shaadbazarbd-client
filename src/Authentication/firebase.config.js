273e7c9981fbcdfe58a9ec2b6b2431f5315f5123
MuhammadTanvirHasan98 <muhammadtanvirhasan98@gmail.com>
2024-12-14 23:42:00 +0600

    fixed bugs in tailwind.config.js and firebase.config.js

--- src/Authentication/firebase.config.js
+++ src/Authentication/firebase.config.js
@@ -2,12 +2,12 @@ import { getAuth } from "firebase/auth";
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD-T-HolV1TCbxNz0l-LNR70WQrGl4LsnA",
-  authDomain: "shaadbazar-bd.firebaseapp.com",
-  projectId: "shaadbazar-bd",
-  storageBucket: "shaadbazar-bd.firebasestorage.app",
-  messagingSenderId: "458087512495",
-  appId: "1:458087512495:web:884f2399ee7f89be88175a"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
